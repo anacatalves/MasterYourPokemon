8099bcd390bf54debc17be79b015e9d3993a8092
anacatalves <ana.catarina.24@hotmail.com>
2021-11-30 15:43:51 +0000

    search-bar

--- public/js/body1.js
+++ public/js/body1.js
@@ -31,9 +31,42 @@ renderItems();
 
 // SEARCH BAR FUNCTION
 
+// DECLARE VARIABLES
+let searchButton = document.querySelector(".search-button img");
 
+let searchedItem = null;
+let matchItem = null; 
 
 function searchItem (){
+    searchedItem = document.querySelector(".search-bar").value;
 
+    matchItem = pokemons.results.find( item => item.name === searchedItem)
+    
+    if (matchItem === undefined) {
+        itemsE1.innerHTML = `<div class ="undefined"> 💔 Pokémon not found in Pokédex 💔 </div>`
+    }
+    else {
+        itemsE1.innerHTML = `
+            <div class="item">
+                        <div class="item-container">
+
+                            <figure class="item-img">
+                                <img src="${matchItem.sprites.front_default}">
+                            </figure> 
+
+                            <div class="item-name">
+                                <h1>${matchItem.name}</h1>
+                                <p>${matchItem.types[0].type.name}</p>
+                            </div>
+                            
+                            <button class="button-select">
+                                <img class ="next" src="img/next-icon.png">
+                            </button>
+
+                        </div>
+                    </div>
+            `
+    }
 }
 
+searchButton.addEventListener('click', searchItem);
--- public/js/changeScreens.js
+++ public/js/changeScreens.js
@@ -2,9 +2,33 @@
 let body1 = document.querySelector(".body1");
 let body2 = document.querySelector(".body2");
 let buttonSelect = document.querySelectorAll(".button-select");
-let buttonExit = document.querySelectorAll(".exit");
+let buttonExit = document.querySelector(".exit");
 
-function changeScreens() {
+function toggleDetailScreen(showDetailScreen) {
+    if(showDetailScreen) {
+        body2.style.display = "block"
+        body1.style.display = "none"
+    } else {
+        body1.style.display = "block"
+        body2.style.display = "none"
+    }
+}
+
+buttonSelect.forEach( (element) => {
+    element.addEventListener('click', function() { toggleDetailScreen(true) })
+})
+
+buttonExit.addEventListener('click', function () { toggleDetailScreen(false) })
+
+
+
+
+
+
+
+// ANOTHER WAY TO DO IT:
+
+/* function changeScreens() {
     if(body1.style.display == "block") {
         body1.style.display = "none"
     } 
@@ -35,21 +59,4 @@ function updateEventListeners() {
     }) 
 }
 
-updateEventListeners();
-
-
-
-//Another way to do it:
-
-/* function toggleDetailScreen(showDetailScreen) {
-    if(showDetailScreen) {
-        body2.style.display = "block"
-        body1.style.display = "none"
-    } else {
-        body1.style.display = "block"
-        body2.style.display = "none"
-    }
-} */
-
-// and add onclick events on the buttons html:
-// true for the arrow, false for the exit.
+updateEventListeners(); */
